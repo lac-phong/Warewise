1fb45e23ecc9c69e41d5c85dde08092af2670936
lac-phong <quanglac03@gmail.com>
2024-05-04 23:48:26 -0700

    fixed backend for login and register

--- server/app.js
+++ server/app.js
@@ -2,11 +2,8 @@ import cors from 'cors'
 import express from 'express'
 
 import { 
-    insertAccount, 
-    getAccount,
-  
-    getBusiness,
     getBusinesses,
+    getBusinessId,
     insertBusiness,
     updateBusiness,
     deleteBusiness,
@@ -31,14 +28,14 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/register', async (req, res) => {
-    const { username, password } = req.body
-    const user = await insertAccount( username, password )
+    const { username, password, business_name } = req.body
+    const user = await insertBusiness( username, password, business_name )
     res.status(201).send(user)
 })
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body
-    const user = await getAccount( username, password )
+    const user = await getBusinessId( username, password )
     res.send(user)
 })
 
@@ -49,25 +46,11 @@ app.get("/businesses", async (req, res) => {
     res.send(businesses)
 })
 
-// EXTERNAL: get specific business
-app.get("/business/:business_id", async (req, res) => {
-    const id = req.params.business_id
-    const business = await getBusiness(id)
-    res.send(business)
-})
-
-// EXTERNAL: insert specific business
-app.post("/business", async (req, res) => {
-    const { business_id, account_id, business_name } = req.body
-    const business = await insertBusiness(business_id, account_id, business_name)
-    res.status(201).send(business)
-})
-
 // EXTERNAL: updating specific business
 app.put("/business/:business_id", async (req, res) => {
-    const { account_id, business_name } = req.body;
+    const { business_name } = req.body;
     const business_id = req.params.business_id;
-    const updatedBusiness = await updateBusiness(business_id, account_id, business_name);
+    const updatedBusiness = await updateBusiness(business_id, business_name);
     res.send(updatedBusiness);
 });
 
